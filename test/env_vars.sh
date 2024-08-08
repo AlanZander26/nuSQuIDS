cf948e25a0bbb5a850c23b48a5420f2f3482db7f
agurruty <agurruty1205@gmail.com>
2024-08-08 11:32:42 +0200

    added readme

--- test/env_vars.sh
+++ test/env_vars.sh
@@ -1,7 +1,7 @@
 
 export CXX="g++"
-export CFLAGS=" -I/home/alan/IceCube_Analysis/nuSQuIDS/inlude -Wno-abi -I/usr/local/include  -I/home/alan/anaconda3/include"
+export CFLAGS=" -I/home/aurruty/Master/MasterThesis/nuSQuIDS/inlude -Wno-abi -I/usr/local/include  -I/usr/include/hdf5/serial"
 export CXXFLAGS=" -std=c++11"
-export LDFLAGS=" -L/home/alan/IceCube_Analysis/nuSQuIDS/lib -Wl,-rpath -Wl,/home/alan/IceCube_Analysis/nuSQuIDS/lib -lnuSQuIDS -lpthread -L/usr/local/lib -lSQuIDS -lgsl -lgslcblas -lm -lgsl -lgslcblas -lm -L/home/alan/anaconda3/lib -lhdf5_hl -lhdf5 -L/home/alan/anaconda3/lib -Wl,-O2 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,--disable-new-dtags -Wl,--gc-sections -Wl,-rpath,/home/alan/anaconda3/lib -Wl,-rpath-link,/home/alan/anaconda3/lib -L/home/alan/anaconda3/lib -lrt -lpthread -lz -ldl -lm -Wl,-rpath -Wl,/home/alan/anaconda3/lib"
+export LDFLAGS=" -L/home/aurruty/Master/MasterThesis/nuSQuIDS/lib -Wl,-rpath -Wl,/home/aurruty/Master/MasterThesis/nuSQuIDS/lib -lnuSQuIDS -lpthread -L/usr/local/lib -lSQuIDS -lgsl -lgslcblas -lm -lgsl -lgslcblas -lm -L/usr/lib/x86_64-linux-gnu/hdf5/serial -lhdf5_hl -lhdf5 -lpthread -lsz -lz -ldl -lm -Wl,-rpath -Wl,/usr/lib/x86_64-linux-gnu/hdf5/serial"
 
-export LD_LIBRARY_PATH="/home/alan/IceCube_Analysis/nuSQuIDS/lib:/usr/local/lib:/usr/lib/x86_64-linux-gnu:/home/alan/anaconda3/lib"
+export LD_LIBRARY_PATH="/home/aurruty/Master/MasterThesis/nuSQuIDS/lib:/usr/local/lib:/usr/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/hdf5/serial"
