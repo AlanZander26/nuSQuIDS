261839deaa6e0a695b46cb34c2fe495e37d1f71f
Juan Agustin Urruty <agurruty1205@gmail.com>
2024-08-27 16:00:46 +0200

    Modified main.cpp to take single parameters and write to hdf5

--- Python/src/implement_TeVSGT.py
+++ Python/src/implement_TeVSGT.py
@@ -1,64 +1,76 @@
 import numpy as np
 import os
 import subprocess
+import h5py
 
-
-def implement_model(model_type, params_ranges, E_range, medium_list, initial_flux_ratios, neutrino_type="neutrino", NormalOrdering=True, Nen=1000, Nen_grid=200):
+def implement_model(model_type, params_ranges, E_range, medium_list, initial_flux_ratios, neutrino_type="neutrino", NormalOrdering=True, Nen=200, Nen_grid=100):
     
     str_sed = '1c\\#define USE_' + model_type.upper()  
     nuSQuIDS_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     TEVSGT_PATH = nuSQuIDS_PATH + "/TeVSGT"
     
-
-
-
     subprocess.run(
         ["sed", "-i", str_sed, "main.cpp"],
         cwd=TEVSGT_PATH,
         text=True
     )
     
-
-    
+    # Convert parameters for the models into their correct types
     for sublist in params_ranges:
-        sublist[0] = float(sublist[0])
-        sublist[1] = float(sublist[1])
-        sublist[2] = int(sublist[2])
+        sublist[0] = float(sublist[0])  # a or N
+        sublist[1] = float(sublist[1])  # m0
+        if len(sublist) == 3:
+            sublist[2] = float(sublist[2])  # mu (if applicable)
         
-    tuple_shape = ()
-    for sublist in params_ranges[::-1]:
-        tuple_shape += tuple([sublist[2]])
-    tuple_shape += tuple([Nen, 3+1])
-    
-    
-    params_ranges = [item for sublist in params_ranges for item in sublist]
+    # Process energy range
     E_range = np.array(E_range, dtype=float)
     E_min, E_max = E_range
+    
+    # Get medium and medium parameter details
     medium = medium_list[0]
     medium_param_range = medium_list[-1]
     medium_param_min = float(medium_param_range[0])
     medium_param_max = float(medium_param_range[1])
     N_medium_param = int(medium_param_range[2])
+    
+    # Convert initial flux ratios
     initial_flux_ratios = np.array(initial_flux_ratios, dtype=float)
+    
+    # Convert NormalOrdering to string representation
     NormalOrdering = str(NormalOrdering).lower()
     
     # Construct the input string for the bash script
     input_str = ""
-    for param_input in params_ranges:
-        input_str += f"{param_input}\n"
+    for sublist in params_ranges:
+        for param in sublist:
+            input_str += f"{param}\n"
+    
     input_str += f"{E_min}\n{E_max}\n{medium}\n{medium_param_min}\n{medium_param_max}\n{N_medium_param}\n{neutrino_type}\n{NormalOrdering}\n{Nen_grid}\n{Nen}\n"
+    
     for ratio in initial_flux_ratios:
         input_str += f"{ratio}\n"
     
     # Run the bash script and provide the input
     subprocess.run(
         ["bash", f"{TEVSGT_PATH}/run_commands.sh"],
-
-    input=input_str,
-    text=True
+        input=input_str,
+        text=True
     )
     
-    tuple_shape = tuple([N_medium_param]) + tuple_shape
-    data_file = os.path.expanduser(f"{TEVSGT_PATH}/fluxes_flavor.txt")
-    data = np.loadtxt(data_file).reshape(tuple_shape)
-    return data
+    
+    # Print the output data
+    hdf5_file = os.path.expanduser(f"{TEVSGT_PATH}/outputfile.hdf5")
+    with h5py.File(hdf5_file, 'r') as hdf_file:
+        # Function to recursively print the structure of the HDF5 file
+        def print_structure(name, obj):
+            obj_type = "Group" if isinstance(obj, h5py.Group) else "Dataset"
+            print(f"{name}: {obj_type}")
+            
+            # If it's a dataset, print its shape and dtype
+            if isinstance(obj, h5py.Dataset):
+                print(f"  Shape: {obj.shape}, DataType: {obj.dtype}")
+
+        # Iterate over all objects in the file and print their structure
+        hdf_file.visititems(print_structure)
+    
+    return
--- TeVSGT/main.cpp
+++ TeVSGT/main.cpp
@@ -1,4 +1,4 @@
-#define USE_SM_COPIES
+#define USE_ADD
 // USE_ADD (ADD), USE_SM_COPIES (SM_Copies), USE_SM (SM)
 #include <vector>
 #include <iostream>
@@ -25,34 +25,19 @@ int main () {
   squids::Const units;
 
 #ifdef USE_ADD
-  double a_min, a_max; // Radius range of largest extra dimension [micro m]
-  unsigned int N_a; // # of bins in dimension "a"
-  double m0_min, m0_max; // Mass range of lightest neutrino in the SM [eV]
-  unsigned int N_m0; // # of bins in dimension "m0"
-  std::cin >> a_min >> a_max >> N_a >> m0_min >> m0_max >> N_m0;
+  double a; // Radius of largest extra dimension [micro m]
+  double m0; // Mass of lightest neutrino in the SM [eV]
+  std::cin >> a >> m0;
   unsigned int N_KK = 2;
   unsigned int numneu = 3*(N_KK + 1); 
 
-  std::vector<double> a_vec = linspace_vec(a_min, a_max, N_a);
-  std::vector<double> m0_vec = linspace_vec(m0_min, m0_max, N_m0);
-  double a;
-  double m0;
 #elif defined(USE_SM_COPIES)
-  double N_min, N_max; // Range of number of SM copies
-  unsigned int N_N; // # of bins in dimension "N"
-  double mu_min, mu_max; // Range of the mass factor
-  unsigned int N_mu; // # of bins in dimension "mu"
-  double m0_min, m0_max; // Mass range of lightest neutrino in the SM [eV]
-  unsigned int N_m0; // # of bins in dimension "m0"
-  std::cin >> N_min >> N_max >> N_N >> mu_min >> mu_max >> N_mu >> m0_min >> m0_max >> N_m0;
+  double N; // Number of SM copies
+  double mu; // Mass factor
+  double m0; // Mass of lightest neutrino in the SM [eV]
+  std::cin >> N >> mu >> m0;
   unsigned int numneu = 6;
 
-  std::vector<double> N_vec = linspace_vec(N_min, N_max, N_N);
-  std::vector<double> mu_vec = linspace_vec(mu_min, mu_max, N_mu);
-  std::vector<double> m0_vec = linspace_vec(m0_min, m0_max, N_m0);
-  double N;
-  double mu;
-  double m0;
 #elif defined(USE_SM)
   unsigned int numneu = 3;
 #endif
@@ -110,143 +95,89 @@ int main () {
 
   std::vector<double> medium_param_vec = linspace_vec(medium_param_min, medium_param_max, N_medium_param);
   double medium_param;
-  //In this part we will save the values in a txt file to be able to plot or manipulate later.
-  //Notice that this is not going to have all the information about the quantum evolution, for that 
-  //we need to save the information using the HDF5 writing function.
-  std::ofstream file("fluxes_flavor.txt");
 
   for(int i = 0; i < N_medium_param; ++i) {
     medium_param = medium_param_vec[i];
 
 #ifdef USE_ADD
-    for(int j = 0; j < N_m0; ++j) {
-        m0 = m0_vec[j];
-        for(int k = 0; k < N_a; ++k) {
-            a = a_vec[k];
-            nuSQUIDS_ADD nus(logspace(E_min*units.GeV,E_max*units.GeV,N_energy_grid), N_KK, a, m0, NormalOrdering, neutrino_type);
+    nuSQUIDS_ADD nus(logspace(E_min*units.GeV,E_max*units.GeV,N_energy_grid), N_KK, a, m0, NormalOrdering, neutrino_type);
 
 #elif defined(USE_SM_COPIES)
-    for(int j = 0; j < N_m0; ++j) {
-        m0 = m0_vec[j];
-        for(int k = 0; k < N_mu; ++k) {
-            mu = mu_vec[k];
-            for(int l = 0; l < N_N; ++l) {
-                N = N_vec[l];
-                nuSQUIDS_SM_Copies nus(logspace(E_min*units.GeV,E_max*units.GeV,N_energy_grid), N, mu, m0, NormalOrdering, numneu, neutrino_type);
+    nuSQUIDS_SM_Copies nus(logspace(E_min*units.GeV,E_max*units.GeV,N_energy_grid), N, mu, m0, NormalOrdering, numneu, neutrino_type);
 
 #elif defined(USE_SM)
     nuSQUIDS nus(logspace(E_min*units.GeV,E_max*units.GeV,N_energy_grid), numneu, neutrino_type, false);
 
 #endif
 
-          if (medium == "vacuum") {
-            double L; // Baseline [km]
-            L = medium_param;
+    if (medium == "vacuum") {
+      double L; // Baseline [km]
+      L = medium_param;
 
-            const float L_baseline = L*units.km;
-            std::shared_ptr<Vacuum> vacuum = std::make_shared<Vacuum>();
-            std::shared_ptr<Vacuum::Track> track_vacuum = std::make_shared<Vacuum::Track>(L_baseline);
-            nus.Set_Body(vacuum);
-            nus.Set_Track(track_vacuum);
-        
-        } else if (medium == "earth") {
-            double phi; // Zenit angle. phi = acos(-1.) when neutrinos crossing the Earth.
-            phi = std::acos(medium_param);
-            //Here we define the trajectory that the particle follows and the object for more examples
-            // of how construct a track and object look body_track example.
-            //Declaration of the body, EarthAtm is one of the predefined bodies
-            std::shared_ptr<EarthAtm> earth_atm = std::make_shared<EarthAtm>();
-            //Definition of the track, in encodes the trajectory inside the body, here is declared with the zenith angle.
-            auto track_atm = std::make_shared<EarthAtm::Track>(earth_atm->MakeTrack(phi));
-            //We set this in the nusSQuID object.
-            nus.Set_Body(earth_atm);
-            nus.Set_Track(track_atm);
-        } else {
-            std::cout << "Error: invalid medium of propagation." << std::endl;
-        }
-          
-
-          //Here we set the maximum size for the integration step, important for fast or sharp variations of the density.
-          nus.Set_h_max( 500.0*units.km );
-
-          //We set the GSL step function
-          nus.Set_GSL_step(gsl_odeiv2_step_rk4);
-
-          //Setting the numerical precision of gsl integrator.
-          nus.Set_rel_error(1.0e-5);
-          nus.Set_abs_error(1.0e-5);
-
-          //Set true the progress bar during the evolution.
-          //nus.Set_ProgressBar(true);
-
-          //Construct the initial state
-      
-          //E_range is an array that contains all the energies.
-          marray<double,1> E_range = nus.GetERange();
-          //Array that contains the initial state of the system, fist component is energy and second every one of the flavors
-          marray<double,2> inistate{E_range.size(),numneu};
-          double N0 = 1.0e18;
-          // Set a power-law spectra for the neutrinos of the SM with initial flavor flux ratios given in "initial_flux_ratios". BSM iniital flux flavors are set to zero. To change this, change the code below.
-          for ( int i = 0 ; i < inistate.extent(0); i++){
-              for ( int k = 0; k < 3; k ++){
-                inistate[i][k] = initial_flux_ratios[k]*N0*pow(E_range[i],-2);
-              }
-              for ( int k = 3; k < inistate.extent(1); k ++){
-                inistate[i][k] = 0.0;
-              }
-          } 
-
-
-          //Set the initial state in nuSQuIDS object
-          nus.Set_initial_state(inistate,flavor);
-          //Propagate the neutrinos in the earth for the path defined in path
-          nus.EvolveState();
-        
-          //number of energies we want the result, notice that this can be larger than the number of the internal grid of 
-          //the nuSQuIDS object, a linear interpolation between the quantum density matrices in the interaction picture is used
-          //and vacuum oscillations are solved analytically for the given energy.
-          double lEmin=std::log10(E_min);
-          double lEmax=std::log10(E_max);
-          std::vector<double> lE_vec = linspace_vec(lEmin, lEmax, Nen);
-          double lE;
-
-          #ifdef USE_ADD
-          file << "# Energy[GeV] Prob_e Prob_mu Prob_tau " << "(a [um] = " << a << ", m0 [eV] = " << m0 << ", medium_param = " << medium_param << ")" << std::endl;
-
-          #elif defined(USE_SM_COPIES)
-          file << "# Energy[GeV] Prob_e Prob_mu Prob_tau " << "(N = " << N << ", mu = " << mu << ", m0 [eV] = " << m0 << ", medium_param = " << medium_param << ")" << std::endl;
-
-          #elif defined(USE_SM)
-          file << "# Energy[GeV] Prob_e Prob_mu Prob_tau " << "(medium_param = " << medium_param << ")" << std::endl;
-
-          #endif
-          for(double i=0; i<Nen; i++){
-            lE = lE_vec[i];
-            double E=pow(10.0,lE); // This expression is dimensionless of the form energy/GeV
-            file << E << " ";
-            for(int fl=0; fl<3; fl++){
-              file << " " <<  nus.EvalFlavor(fl, E*units.GeV)/(N0*pow(E*units.GeV,-2));
-            }
-            file << std::endl;
-          }
-        
-#ifdef USE_ADD
-        }
-    }
-#elif defined(USE_SM_COPIES)
-      }
-    }
-  }
-#elif defined(USE_SM)
-#endif
-  }
-  file.close();
-  std::cout << std::endl <<  "Done! " << std::endl;    
+      const float L_baseline = L*units.km;
+      std::shared_ptr<Vacuum> vacuum = std::make_shared<Vacuum>();
+      std::shared_ptr<Vacuum::Track> track_vacuum = std::make_shared<Vacuum::Track>(L_baseline);
+      nus.Set_Body(vacuum);
+      nus.Set_Track(track_vacuum);
   
+  } else if (medium == "earth") {
+      double phi; // Zenit angle. phi = acos(-1.) when neutrinos crossing the Earth.
+      phi = std::acos(medium_param);
+      //Here we define the trajectory that the particle follows and the object for more examples
+      // of how construct a track and object look body_track example.
+      //Declaration of the body, EarthAtm is one of the predefined bodies
+      std::shared_ptr<EarthAtm> earth_atm = std::make_shared<EarthAtm>();
+      //Definition of the track, in encodes the trajectory inside the body, here is declared with the zenith angle.
+      auto track_atm = std::make_shared<EarthAtm::Track>(earth_atm->MakeTrack(phi));
+      //We set this in the nusSQuID object.
+      nus.Set_Body(earth_atm);
+      nus.Set_Track(track_atm);
+  } else {
+      std::cout << "Error: invalid medium of propagation." << std::endl;
+  }
+    
 
+    //Here we set the maximum size for the integration step, important for fast or sharp variations of the density.
+    nus.Set_h_max( 500.0*units.km );
+
+    //We set the GSL step function
+    nus.Set_GSL_step(gsl_odeiv2_step_rk4);
+
+    //Setting the numerical precision of gsl integrator.
+    nus.Set_rel_error(1.0e-5);
+    nus.Set_abs_error(1.0e-5);
+
+    //Set true the progress bar during the evolution.
+    //nus.Set_ProgressBar(true);
+
+    //Construct the initial state
+
+    //E_range is an array that contains all the energies.
+    marray<double,1> E_range = nus.GetERange();
+    //Array that contains the initial state of the system, fist component is energy and second every one of the flavors
+    marray<double,2> inistate{E_range.size(),numneu};
+    double N0 = 1.0e18;
+    // Set a power-law spectra for the neutrinos of the SM with initial flavor flux ratios given in "initial_flux_ratios". BSM iniital flux flavors are set to zero. To change this, change the code below.
+    for ( int i = 0 ; i < inistate.extent(0); i++){
+        for ( int k = 0; k < 3; k ++){
+          inistate[i][k] = initial_flux_ratios[k]*N0*pow(E_range[i],-2);
+        }
+        for ( int k = 3; k < inistate.extent(1); k ++){
+          inistate[i][k] = 0.0;
+        }
+    } 
+
+
+    //Set the initial state in nuSQuIDS object
+    nus.Set_initial_state(inistate,flavor);
+    //Propagate the neutrinos in the earth for the path defined in path
+    nus.EvolveState();
+
+    // Naming the output file
+    std::string output_file = "outputfile.hdf5";
+    nus.WriteStateHDF5(output_file);
+  }
+
+  std::cout << std::endl <<  "Done! " << std::endl;    
   return 0;
-
 }
-
-
-
