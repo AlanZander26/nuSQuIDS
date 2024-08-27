d75229f7f4a6e21fe7524df41cdf3d61b0449dd6
Juan Agustin Urruty <agurruty1205@gmail.com>
2024-08-27 13:40:18 +0200

    Changed Compiler Flag from O3 to O2

--- test/env_vars.sh
+++ test/env_vars.sh
@@ -1,7 +1,7 @@
 
 export CXX="g++"
-export CFLAGS=" -I/home/aurruty/Master/MasterThesis/nuSQuIDS/inlude -Wno-abi -I/usr/local/include  -I/usr/include/hdf5/serial"
+export CFLAGS=" -I/home/agurruty/Projects/MasterThesis/nuSQuIDS/inlude -Wno-abi -I/usr/local/include  -I/usr/include/hdf5/serial"
 export CXXFLAGS=" -std=c++11"
-export LDFLAGS=" -L/home/aurruty/Master/MasterThesis/nuSQuIDS/lib -Wl,-rpath -Wl,/home/aurruty/Master/MasterThesis/nuSQuIDS/lib -lnuSQuIDS -lpthread -L/usr/local/lib -lSQuIDS -lgsl -lgslcblas -lm -lgsl -lgslcblas -lm -L/usr/lib/x86_64-linux-gnu/hdf5/serial -lhdf5 -lhdf5_hl"
+export LDFLAGS=" -L/home/agurruty/Projects/MasterThesis/nuSQuIDS/lib -Wl,-rpath -Wl,/home/agurruty/Projects/MasterThesis/nuSQuIDS/lib -lnuSQuIDS -lpthread -L/usr/local/lib -lSQuIDS -lgsl -lgslcblas -lm -lgsl -lgslcblas -lm -L/usr/lib/x86_64-linux-gnu/hdf5/serial -lhdf5 -lhdf5_hl"
 
-export LD_LIBRARY_PATH="/home/aurruty/Master/MasterThesis/nuSQuIDS/lib:/usr/local/lib:/usr/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/hdf5/serial"
+export LD_LIBRARY_PATH="/home/agurruty/Projects/MasterThesis/nuSQuIDS/lib:/usr/local/lib:/usr/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/hdf5/serial:/home/agurruty/Projects/SciComputing/root_install/lib"
